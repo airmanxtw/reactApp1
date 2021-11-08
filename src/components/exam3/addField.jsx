0e1f6e0b6a4b15d90b14c3c1b98accd2be610fc9
airmanxtw <airmanx@stust.edu.tw>
2021-11-08 08:28:17 +0800

    add field

--- src/components/exam3/addField.jsx
+++ src/components/exam3/addField.jsx
@@ -1,27 +1,29 @@
 import React, { useState } from "react";
 function AddField() {
-    // const [counter, setCounter] = useState(0);
-    // const [cars, setCars] = useState([
-    //     { key: 0, id: "123", desc: "hello" }
-    // ]);
-    // let addCounter = () => {
-    //     setCounter(counter + 1);
-    // };
-    // let add = () => {
-    //     addCounter();
-    //     setCars(cars => [...cars, { key: counter, id: '', desc: '' }]);
-    // };
-    // let del = (key) => {
-    //     setCars(cars => [cars.filter(item => item.key != key)]);
-    // };
-    let test = () => {
-        alert('hllo2');
+    const [counter, setCounter] = useState(0);
+    const [cars, setCars] = useState([]);
+    let addCounter = () => {
+        setCounter(counter + 1);
+    };
+    let add = () => {
+        addCounter();
+        setCars(cars => [...cars, { key: counter, id: '', desc: '' }]);
+    };
+    let del = (key) => {
+        setCars(cars.filter(item => item.key != key));
     };
     return (
         <div>
-            <button onClick={(e) => test()}>+</button>
+            {
+                cars.map(c => <div key={c.key}>
+                    <input placeholder="車型代碼" />
+                    <input placeholder="車型描述" />
+                    <button onClick={() => del(c.key)}>刪除</button>
+                </div>
+                )
+            }
+            <button onClick={() => add()}>+</button>
         </div>
     );
 }
-
 export default AddField;
