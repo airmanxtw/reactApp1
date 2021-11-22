5c43e6d51714ee348452bfede5a84e7faceb6e8c
airmanxtw <airmanx@stust.edu.tw>
2021-11-23 00:04:18 +0800

    mui

--- src/components/exam6/testui.jsx
+++ src/components/exam6/testui.jsx
@@ -1,8 +1,13 @@
-import { Button } from "@material-ui/core";
+import { AppBar, Button, Toolbar } from "@material-ui/core";
 import { ArrowBack, DeleteRounded, DeleteSweepTwoTone } from "@material-ui/icons";
 import React from "react";
 let TestUi = function () {
     return <div>
+        <AppBar position="absolute">
+            <Toolbar>
+                <h1>Hello</h1>
+            </Toolbar>
+        </AppBar>
         <Button variant="contained" startIcon={<ArrowBack />} >Hello</Button>
     </div>
 }
